a2ce1902bb59545e06f1d37f1fd8388aadd1eb51
GOLDER303 <szymonstepien@interia.pl>
2023-03-07 23:01:31 +0100

    Add technical support form

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -1,5 +1,50 @@
+import React from "react"
+import { createSupportSubmission } from "../services/SupportService"
+
 const SupportPage: React.FC = () => {
-    return <h1>Support Page</h1>
+    const nameInputRef = React.useRef<HTMLInputElement>(null)
+    const surnameInputRef = React.useRef<HTMLInputElement>(null)
+    const emailInputRef = React.useRef<HTMLInputElement>(null)
+    const descriptionInputRef = React.useRef<HTMLTextAreaElement>(null)
+
+    const [submissionMessage, setSubmissionMessage] = React.useState<string>("")
+
+    const handleSubmit = async () => {
+        if (!nameInputRef.current || !surnameInputRef.current || !emailInputRef.current || !descriptionInputRef.current) {
+            return
+        }
+
+        const createSupportSubmissionResponse = await createSupportSubmission(nameInputRef.current.value, surnameInputRef.current.value, emailInputRef.current.value, descriptionInputRef.current.value)
+
+        if (createSupportSubmissionResponse.error) {
+            setSubmissionMessage("Coś poszło nie tak")
+        } else {
+            setSubmissionMessage("Zgłoszenie wysłane")
+        }
+    }
+
+    return (
+        <>
+            <h1>Support Page</h1>
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault()
+                    handleSubmit()
+                }}
+            >
+                <label htmlFor="name">Imię: </label>
+                <input ref={nameInputRef} type="text" name="name" id="name" /> <br />
+                <label htmlFor="surname">Nazwisko: </label>
+                <input ref={surnameInputRef} type="text" name="surname" id="surname" /> <br />
+                <label htmlFor="email">E-mail: </label>
+                <input ref={emailInputRef} type="email" name="email" id="email" /> <br />
+                <label htmlFor="description">Opis zgłoszenia: </label> <br />
+                <textarea ref={descriptionInputRef} name="description" id="description" cols={30} rows={10}></textarea> <br />
+                <button type="submit">Wyślij zgłoszenie</button>
+            </form>
+            <div>{submissionMessage}</div>
+        </>
+    )
 }
 
 export default SupportPage
---
+++ src/services/SupportService.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosError } from "axios"
+import ApiErrorResponseDTO from "../ts/interfaces/ApiErrorResponseDTO"
+import GeneralApiResponseDTO from "../ts/interfaces/GeneralApiResponseDTO"
+
+export const createSupportSubmission = async (name: string, surname: string, email: string, description: string): Promise<GeneralApiResponseDTO<string>> => {
+    try {
+        const response = await axios.post("http://localhost:8080/support/submissions", {
+            name,
+            surname,
+            email,
+            message: description,
+        })
+
+        return {}
+    } catch (err) {
+        const error = err as AxiosError
+
+        console.log(error)
+
+        const data = error.response?.data as ApiErrorResponseDTO
+
+        return { error: data.status }
+    }
+}
