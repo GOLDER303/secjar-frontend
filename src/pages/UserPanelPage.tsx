a79a65f81e046d15f80e755970c4a5d2eb8a1ea4
GOLDER303 <szymonstepien@interia.pl>
2023-03-13 14:49:09 +0100

    Remove commented code and console logs

--- src/pages/UserPanelPage.tsx
+++ src/pages/UserPanelPage.tsx
@@ -1,46 +1,48 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { IsLoggedInContext, IsLoggedInContextType } from "../contexts/IsLoggedInContext"
-import FileEntry from "../components/FileEntry";
-import {getMyFiles} from "../services/FileService";
-import ApiFileResponseDTO from "../ts/interfaces/ApiFileResponseDTO";
+import FileEntry from "../components/FileEntry"
+import { getMyFiles } from "../services/FileService"
+import ApiFileResponseDTO from "../ts/interfaces/ApiFileResponseDTO"
 
 const UserPanelPage: React.FC = () => {
-    const { isUserLoggedIn } = React.useContext(IsLoggedInContext) as IsLoggedInContextType;
-    const [filesArray, setFilesArray] = React.useState([<></>]);
-    let filesData : [ApiFileResponseDTO];
-    console.log(filesArray);
+    const { isUserLoggedIn } = React.useContext(IsLoggedInContext) as IsLoggedInContextType
+    const [filesArray, setFilesArray] = React.useState([<></>])
+    let filesData: [ApiFileResponseDTO]
 
     useEffect(() => {
         getMyFiles().then((fileData) => {
             if (fileData.error) {
-                console.log("fileData error")
+                //TODO: handle error
             }
             if (fileData.data) {
-                filesData = fileData.data;
-                setFilesArray(filesData.map((val) => {
-                    return <FileEntry data={val} />
-                }));
+                filesData = fileData.data
+                setFilesArray(
+                    filesData.map((val) => {
+                        return <FileEntry data={val} />
+                    })
+                )
             }
-        });
-        console.log(filesArray);
+        })
     }, [])
 
     const showPage = () => {
-        return (
-            <>
-                {filesArray.length > 0 && filesArray[0] != <></>
-                    ? filesArray
-                    : <p>There is no files present. Try to upload a file.</p>}
-            </>
-        )
+        return <>{filesArray.length > 0 && filesArray[0] != <></> ? filesArray : <p>There is no files present. Try to upload a file.</p>}</>
     }
 
     return (
         <>
             <h1>User Panel Page</h1>
-            {isUserLoggedIn ? <><h2>Hello</h2>{showPage()}</>  : <h2>You need to <Link to={"/login"}>login</Link> first</h2>}
-
+            {isUserLoggedIn ? (
+                <>
+                    <h2>Hello</h2>
+                    {showPage()}
+                </>
+            ) : (
+                <h2>
+                    You need to <Link to={"/login"}>login</Link> first
+                </h2>
+            )}
         </>
     )
 }
