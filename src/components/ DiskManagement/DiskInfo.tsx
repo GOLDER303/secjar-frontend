67e0ab8fd603da3aaa228b0b94bb9bbe99519dfe
Bartlomiej-Kozlowski <104801@stud.uz.zgora.pl>
2023-03-16 16:10:28 +0100

    Add Disk management functionality Add Mime types Addidion and Deletion functionality

---
+++ src/components/ DiskManagement/DiskInfo.tsx
@@ -0,0 +1,96 @@
+import React, {useEffect} from "react"
+import DiskInfoDTO from "../../ts/interfaces/DiskInfoDTO"
+import MimeTypeList from "./MimeTypeList"
+
+interface DiskInfoProps {
+    diskInfoDTO: DiskInfoDTO | null
+    handleMaxUserSessionTimeEdit: (value: number) => void
+    handleMimeTypeDelete: (typeName: string) => void
+    handleMimeTypeAdd: (typeName: string) => void
+}
+
+const DiskInfo: React.FC<DiskInfoProps> = ({ diskInfoDTO, handleMaxUserSessionTimeEdit, handleMimeTypeDelete, handleMimeTypeAdd }) => {
+    const [isMaxUserSessionTimeEdited, setIsMaxUserSessionTimeEdited] = React.useState(false)
+    const [maxUserSessionTime, setMaxUserSessionTime] = React.useState<number | null>(null)
+    const [mimeTypeAddValue, setMimeTypeAddValue] = React.useState<string | null>(null)
+
+    const maxUserSessionTimeMinutes = maxUserSessionTime && Math.round(maxUserSessionTime / 60_000) || 0
+
+    useEffect(() => {
+        if (diskInfoDTO){
+            setMaxUserSessionTime(diskInfoDTO.maxUserSessionTime)
+        }
+    }, [isMaxUserSessionTimeEdited, diskInfoDTO])
+
+    const handleSubmit = () => {
+        if (maxUserSessionTime != null) {
+            handleMaxUserSessionTimeEdit(maxUserSessionTime)
+            setIsMaxUserSessionTimeEdited(false)
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                Maksymalny czas trwania sesji użytkownika:
+                {isMaxUserSessionTimeEdited ? (
+                    <form
+                        onSubmit={(event) => {
+                            event.preventDefault()
+                            handleSubmit()
+                        }}
+                    >
+                        <input
+                            type="number"
+                            value={maxUserSessionTimeMinutes}
+                            onChange={(e) => {
+                                setMaxUserSessionTime(parseInt(e.target.value) * 60_000)
+                            }}
+                        />
+                        <input type="submit"/>
+                        <button
+                            onClick={() => {
+                                setIsMaxUserSessionTimeEdited(false)
+                            }}
+                        >
+                            Anuluj
+                        </button>
+                    </form>
+                ) : (
+                    <>
+                        {maxUserSessionTimeMinutes}
+                        <button
+                            onClick={() => {
+                                setIsMaxUserSessionTimeEdited(true)
+                            }}
+                        >
+                            Edytuj
+                        </button>
+                    </>
+                )}
+            </div>
+            <div>Niedozwolone typy MIME:<br />
+                <MimeTypeList
+                    mimeTypes={diskInfoDTO && diskInfoDTO.disallowedMimeTypes || []}
+                    handleMimeTypeDelete={handleMimeTypeDelete}
+                />
+                Dodaj typ:
+                <input
+                    type="text"
+                    onChange={(e) => {
+                        e.target && setMimeTypeAddValue(e.target.value)
+                    }}
+                />
+                <button
+                    onClick={() => {
+                        handleMimeTypeAdd(mimeTypeAddValue || "")
+                    }}
+                >
+                    Dodaj
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default DiskInfo
--- src/pages/AdminPanelPage/Tabs/DiskManagementTab.tsx
+++ src/pages/AdminPanelPage/Tabs/DiskManagementTab.tsx
@@ -1,9 +1,76 @@
-import React from "react"
+import React, {useEffect} from "react"
+import DiskInfo from "../../../components/ DiskManagement/DiskInfo"
+import DiskInfoDTO from "../../../ts/interfaces/DiskInfoDTO"
+import {editDiskInfo, getDiskInfo} from "../../../services/DiskManagementService"
 
 const DiskManagementTab: React.FC = () => {
+    const [diskInfo, setDiskInfo] = React.useState<DiskInfoDTO | null>(null)
+
+    useEffect(() => {
+        refreshDiskInfo()
+    }, [])
+
+
+    const refreshDiskInfo = async () => {
+        const response = await getDiskInfo()
+        if (response.data) {
+            setDiskInfo(response.data)
+        } else {
+            //TODO: handle error
+        }
+    }
+
+    const handleMaxUserSessionTimeEdit = async (value: number) => {
+        if (diskInfo){
+            const updatedDiskInfo = {
+                maxUserSessionTime: value,
+                disallowedMimeTypes: diskInfo.disallowedMimeTypes,
+            } as DiskInfoDTO
+            await editDiskInfo(updatedDiskInfo)
+            // TODO: handle errors
+            refreshDiskInfo()
+        }
+    }
+
+    const handleMimeTypeAdd = async (typeName: string) => {
+        if (diskInfo){
+            if (diskInfo.disallowedMimeTypes.findIndex((value) => value == typeName) == -1){
+                const updatedMimeTypes = diskInfo.disallowedMimeTypes.concat(typeName)
+                const updatedDiskInfo = {
+                    maxUserSessionTime: diskInfo.maxUserSessionTime,
+                    disallowedMimeTypes: updatedMimeTypes,
+                } as DiskInfoDTO
+                await editDiskInfo(updatedDiskInfo)
+                // TODO: handle errors
+                refreshDiskInfo()
+            }
+        }
+    }
+
+    const handleMimeTypeDelete = async (typeName: string) => {
+        if (diskInfo){
+            const updatedMimeTypes = diskInfo.disallowedMimeTypes.filter((value) => {
+                return value != typeName
+            })
+            const updatedDiskInfo = {
+                maxUserSessionTime: diskInfo.maxUserSessionTime,
+                disallowedMimeTypes: updatedMimeTypes,
+            } as DiskInfoDTO
+            await editDiskInfo(updatedDiskInfo)
+            // TODO: handle errors
+            refreshDiskInfo()
+        }
+    }
+
     return (
         <div>
-            Disk management
+            <h2>Dysk</h2>
+            <DiskInfo
+                diskInfoDTO={diskInfo}
+                handleMimeTypeAdd={handleMimeTypeAdd}
+                handleMaxUserSessionTimeEdit={handleMaxUserSessionTimeEdit}
+                handleMimeTypeDelete={handleMimeTypeDelete}
+            />
         </div>
     )
 }
