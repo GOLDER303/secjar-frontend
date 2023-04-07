b64f62464bdd19fa10274d8d76a829205e61b02d
GOLDER303 <szymonstepien@interia.pl>
2023-04-07 19:47:05 +0200

    Fix bug that displayed error message on password reset confirm page

--- src/pages/PasswordResetPages/ConfirmPasswordResetPage.tsx
+++ src/pages/PasswordResetPages/ConfirmPasswordResetPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { resetUserPassword } from "../../services/PasswordResetService"
 
@@ -9,10 +9,6 @@ const ConfirmPasswordReset: React.FC = () => {
 
     const [searchParams] = useSearchParams()
     const token = searchParams.get("token")
-
-    useEffect(() => {
-        handleSubmit()
-    }, [])
 
     const handleSubmit = async () => {
         if (!token) {
