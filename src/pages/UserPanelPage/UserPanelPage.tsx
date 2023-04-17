ffa584082760c474961616bf1b70cf652b4cf58d
Bartlomiej-Kozlowski <104801@stud.uz.zgora.pl>
2023-04-18 01:57:58 +0200

    Add disk space bar indicating how much space is allocated at user panel

--- src/pages/UserPanelPage/UserPanelPage.tsx
+++ src/pages/UserPanelPage/UserPanelPage.tsx
@@ -5,6 +5,9 @@ import UsernamesUuidsMapProvider from "../../contexts/UsernamesUuidsMapContext"
 import "../../css/UserPanelPage.css"
 import { getFileSystemEntriesInfo } from "../../services/FileSystemEntryInfoService"
 import FileSystemEntryInfoDTO from "../../ts/interfaces/FileSystemEntryInfoDTO"
+import jwt_decode from "jwt-decode"
+import {getUserInfo} from "../../services/UserManagementService"
+import {formatFileSize} from "../../utils/FormatFileSizeUtil"
 
 export type fileSystemEntriesInfoListContextType = { fileSystemEntriesInfoList: FileSystemEntryInfoDTO[]; refreshFileSystemEntriesInfoList: () => void }
 
@@ -14,6 +17,30 @@ const UserPanelPage: React.FC = () => {
     const outlet = useOutlet()
 
     const [fileSystemEntriesInfoList, setFileSystemEntriesInfoList] = React.useState<FileSystemEntryInfoDTO[]>([])
+
+    const [currentDiskSpace, setCurrentDiskSpace] = React.useState<number>(0)
+    const [allowedDiskSpace, setAllowedDiskSpace] = React.useState<number>(0)
+    const [currentDiskSpaceString, setCurrentDiskSpaceString] = React.useState<string>("")
+    const [allowedDiskSpaceString, setAllowedDiskSpaceString] = React.useState<string>("")
+
+    React.useEffect(() => {
+        getUserDiscSpace();
+    }, [])
+    const getUserDiscSpace = async () => {
+        const token = localStorage.getItem("jwt")
+        if (!token) {
+            return
+        }
+        const jwt = jwt_decode(token) as { userUuid: string }
+
+        const response = await getUserInfo(jwt.userUuid) as { data: { currentDiscSpace: number, allowedDiscSpace: number } }
+        const { sizeValue: currentValue, sizeUnit: currentUnit } = formatFileSize(response.data.currentDiscSpace)
+        setCurrentDiskSpace(response.data.currentDiscSpace)
+        setCurrentDiskSpaceString(currentValue.toString() + currentUnit)
+        const { sizeValue: allowedValue, sizeUnit: allowedUnit } = formatFileSize(response.data.allowedDiscSpace)
+        setAllowedDiskSpace(response.data.allowedDiscSpace)
+        setAllowedDiskSpaceString(allowedValue.toString() + allowedUnit)
+    }
 
     const refreshFileSystemEntriesInfoList = async () => {
         const response = await getFileSystemEntriesInfo()
@@ -42,20 +69,41 @@ const UserPanelPage: React.FC = () => {
 
     return (
         <div className="user-panel-page-container">
-            <h1>User Panel Page</h1>
             {isUserLoggedIn ? (
                 <>
                     <nav>
-                        <Link to={"uploaded"}>Przesłane pliki</Link>
-                        <Link to={"favorite"}>Ulubione pliki</Link>
-                        <Link to={"deleted"}>Usunięte pliki</Link>
-                        <Link to={"shared"}>Udostępnione pliki</Link>
-                        <Link to={"settings"}>Ustawienia</Link>
+                        <ul>
+                            <li>
+                                <Link to={"uploaded"}>Przesłane pliki</Link>
+                            </li>
+                            <li>
+                                <Link to={"favorite"}>Ulubione pliki</Link>
+                            </li>
+                            <li>
+                                <Link to={"deleted"}>Usunięte pliki</Link>
+                            </li>
+                            <li>
+                                <Link to={"shared"}>Udostępnione pliki</Link>
+                            </li>
+                            <li>
+                                <Link to={"settings"}>Ustawienia</Link>
+                            </li>
+                        </ul>
                     </nav>
-
-                    <UsernamesUuidsMapProvider>
-                        {outlet ? <Outlet context={{ fileSystemEntriesInfoList, refreshFileSystemEntriesInfoList }} /> : <Navigate to={"uploaded"} />}
-                    </UsernamesUuidsMapProvider>
+                    <div className="progress-bar">
+                        <progress
+                            value={currentDiskSpace}
+                            max={allowedDiskSpace}
+                            unitValue={currentDiskSpaceString}
+                            unitMax={allowedDiskSpaceString}
+                        >
+                        </progress>
+                    </div>
+                    <div className="tab-container">
+                        <UsernamesUuidsMapProvider>
+                            {outlet ? <Outlet context={{ fileSystemEntriesInfoList, refreshFileSystemEntriesInfoList }} /> : <Navigate to={"uploaded"} />}
+                        </UsernamesUuidsMapProvider>
+                    </div>
                 </>
             ) : (
                 <h2>
